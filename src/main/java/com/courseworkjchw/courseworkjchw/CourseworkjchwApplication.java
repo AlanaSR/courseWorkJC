9b28caf05d102a8759947697ad38c4cadead7a8f
AlanaSR <svetlanka.shkola@gmail.com>
2025-02-05 14:54:40 +0500

    initialization

---
+++ src/main/java/com/courseworkjchw/courseworkjchw/CourseworkjchwApplication.java
@@ -0,0 +1,13 @@
+package com.courseworkjchw.courseworkjchw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseworkjchwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseworkjchwApplication.class, args);
+	}
+
+}
